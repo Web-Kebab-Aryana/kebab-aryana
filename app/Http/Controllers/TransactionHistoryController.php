6c39380db6c27ae415d739f1e1f9244238f63ce2
jun <juntesemail@gmail.com>
2024-05-12 16:10:37 +0700

    Transaction-HistoryAPI

--- app/Http/Controllers/TransactionHistoryController.php
+++ app/Http/Controllers/TransactionHistoryController.php
@@ -3,10 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 
-class TransactionHistory extends Controller
+class TransactionHistoryController extends Controller
 {
     // bikin buat selectable date nya, jadi bisa di filter by Day, Week, Month, Year
+    public function view()
+    {
+        $orders = Order::all();
 
+        $transactions = [];
+
+        foreach ($orders as $order) {
+            $total = $order->orderDetails->sum(function($orderDetail){
+                return $orderDetail->menu->price * $orderDetail->quantity;
+            });
+
+            $transaction = [
+                'id'=> $order->id,
+                'customer_name' => $order->customer_name,
+                'total' => $total,
+                'transaction_date' => $order->created_at
+            ];
+
+            $transactions[] = $transaction;
+        }
+
+        return $transactions;
+    }
+
+    public function viewById($id)
+    {
+        $order = Order::find($id);
+
+        $total = $order->orderDetails->sum(function ($orderDetail) {
+            return $orderDetail->menu->price * $orderDetail->quantity;
+        });
+
+        $transaction = [
+            'id' => $order->id,
+            'customer_name' => $order->customer_name,
+            'total' => $total,
+            'transaction_date' => $order->created_at
+        ];
+
+        return $transaction;
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\TransactionHistoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -28,10 +29,15 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/', [OrderController::class, 'store'])->name('orders.store');
         Route::delete('/{id}', [OrderController::class, 'destroy'])->name('orders.destroy');
     });
-});
 
-Route::prefix('menus')->group(function () {
-    Route::post('/', [MenuController::class, 'store'])->name('menus.store');
-    Route::put('/{id}', [MenuController::class, 'update'])->name('menus.update');
-    Route::delete('/{id}', [MenuController::class, 'destroy'])->name('menus.destroy');
+    Route::prefix('menus')->group(function () {
+        Route::post('/', [MenuController::class, 'store'])->name('menus.store');
+        Route::put('/{id}', [MenuController::class, 'update'])->name('menus.update');
+        Route::delete('/{id}', [MenuController::class, 'destroy'])->name('menus.destroy');
+    });
+
+    Route::prefix('transaction')->group(function () {
+        Route::get('/', [TransactionHistoryController::class, 'view'])->name('transaction.view');
+        Route::get('/{id}', [TransactionHistoryController::class, 'viewById'])->name('transaction.viewById');
+    });
 });
