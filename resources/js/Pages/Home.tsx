f947b373bcdebe298ca07c3cd17abe731b893148
Opengheimer <intltcsfleader@gmail.com>
2024-05-25 17:16:00 +0700

    feat: home page initial commit and routing

---
+++ resources/js/Pages/Home.tsx
@@ -0,0 +1,11 @@
+import { PageProps } from "@/types";
+import { Stack, Text } from "@chakra-ui/react";
+export default function Home({ auth }: PageProps) {
+    return (
+        <>
+            <Stack>
+                <Text>ini page home nya</Text>
+            </Stack>
+        </>
+    );
+}
