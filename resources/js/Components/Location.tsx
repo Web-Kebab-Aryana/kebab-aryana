41fcdb1d4aea1069ef25f04eca60733f82a12f1e
Gian Avila <avila.gian111@gmail.com>
2024-05-28 13:24:05 +0700

    feat: location section layout, minus repoonsive

---
+++ resources/js/Components/Location.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box, Heading, Stack, Image, Text, Button } from "@chakra-ui/react";
+
+const Location = () => {
+    return <div>Location</div>;
+};
+
+export default Location;
