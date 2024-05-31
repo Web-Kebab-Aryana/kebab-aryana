a88a78eac7b9deca2d6e7e98ecac02cdcebd89df
Opengheimer <intltcsfleader@gmail.com>
2024-05-31 17:24:27 +0700

    fix: responsiveness

--- resources/js/Components/DivideStats.tsx
+++ resources/js/Components/DivideStats.tsx
@@ -3,48 +3,115 @@ import { Image, Stack, Text, Divider } from "@chakra-ui/react";
 export default function DivideStats() {
     return (
         <>
-            <Stack w={"100vw"}>
-                <Stack>
-                    <Image src="/Divide-Stats/divideStats.svg" />
-                </Stack>
-            </Stack>
-
             <Stack
-                direction={"row"}
+                direction={["column", "column", "row", "row", "row"]}
                 w={"100vw"}
-                h={"16rem"}
+                h={["32rem", "36rem", "16rem", "16rem", "16rem"]}
+                px={["8rem", "8rem", "8rem", "2rem", "8rem"]}
+                py={["2rem", "2rem", "2rem", "2rem", "2rem"]}
                 bgColor={"#FFF6D9"}
                 align={"center"}
+                justify={[
+                    "space-evenly",
+                    "space-evenly",
+                    "space-evenly",
+                    "center",
+                    "center",
+                ]}
+                gap={0}
             >
-                <Stack>
-                    <Text fontWeight={"bold"} fontSize={"6xl"}>
-                        100+
-                    </Text>
-                    <Text fontSize={"3xl"}>Orders/month</Text>
+                <Stack direction={["column", "column", "column", "row", "row"]}>
+                    <Stack align={"center"} px={"2rem"}>
+                        <Text
+                            fontWeight={"bold"}
+                            fontSize={["4xl", "4xl", "4xl", "5xl", "6xl"]}
+                            textColor={"#5F4F3A"}
+                        >
+                            100+
+                        </Text>
+                        <Text
+                            fontSize={["xl", "2xl", "2xl", "3xl", "3xl"]}
+                            textColor={"#5F4F3A80"}
+                            align={"center"}
+                        >
+                            Orders/month
+                        </Text>
+                    </Stack>
+                    <Divider
+                        display={["none", "none", "none", "block", "block"]}
+                        orientation="vertical"
+                        border={"solid 3px"}
+                        rounded={"3xl"}
+                        h={"11rem"}
+                        color={"#5F4F3A33"}
+                    />
+                    <Stack align={"center"} px={"2rem"}>
+                        <Text
+                            fontWeight={"bold"}
+                            fontSize={["4xl", "4xl", "4xl", "5xl", "6xl"]}
+                            textColor={"#5F4F3A"}
+                        >
+                            4.9
+                        </Text>
+                        <Text
+                            fontSize={["xl", "2xl", "2xl", "3xl", "3xl"]}
+                            textColor={"#5F4F3A80"}
+                            align={"center"}
+                        >
+                            Google Score
+                        </Text>
+                    </Stack>
+                    <Divider
+                        display={["none", "none", "none", "block", "block"]}
+                        orientation="vertical"
+                        border={"solid 3px"}
+                        rounded={"3xl"}
+                        h={"11rem"}
+                        color={"#5F4F3A33"}
+                    />
                 </Stack>
-                <Divider
-                    orientation="vertical"
-                    border={"solid 3px"}
-                    rounded={"3xl"}
-                    h={"11rem"}
-                />
-                <Stack>
-                    <Text fontWeight={"bold"} fontSize={"6xl"}>
-                        4.9
-                    </Text>
-                    <Text fontSize={"3xl"}>Orders/month</Text>
-                </Stack>
-                <Divider
-                    orientation="vertical"
-                    border={"solid 3px"}
-                    rounded={"3xl"}
-                    h={"11rem"}
-                />
-                <Stack>
-                    <Text fontWeight={"bold"} fontSize={"6xl"}>
-                        90+
-                    </Text>
-                    <Text fontSize={"3xl"}>Receipts we have</Text>
+
+                <Stack direction={["column", "column", "column", "row", "row"]}>
+                    <Stack align={"center"} px={"2rem"}>
+                        <Text
+                            fontWeight={"bold"}
+                            fontSize={["4xl", "4xl", "4xl", "5xl", "6xl"]}
+                            textColor={"#5F4F3A"}
+                        >
+                            90+
+                        </Text>
+                        <Text
+                            fontSize={["xl", "2xl", "2xl", "3xl", "3xl"]}
+                            textColor={"#5F4F3A80"}
+                            align={"center"}
+                        >
+                            Receipts we have
+                        </Text>
+                    </Stack>
+                    <Divider
+                        display={["none", "none", "none", "block", "block"]}
+                        orientation="vertical"
+                        border={"solid 3px"}
+                        rounded={"3xl"}
+                        h={"11rem"}
+                        color={"#5F4F3A33"}
+                    />
+                    <Stack align={"center"} px={"2rem"}>
+                        <Text
+                            fontWeight={"bold"}
+                            fontSize={["4xl", "4xl", "4xl", "5xl", "6xl"]}
+                            textColor={"#5F4F3A"}
+                        >
+                            69+
+                        </Text>
+                        <Text
+                            fontSize={["xl", "2xl", "2xl", "3xl", "3xl"]}
+                            textColor={"#5F4F3A80"}
+                            align={"center"}
+                        >
+                            Tekkom 22 order
+                        </Text>
+                    </Stack>
                 </Stack>
             </Stack>
         </>
