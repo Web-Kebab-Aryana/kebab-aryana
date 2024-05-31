81cca4d1a47b8de82a6530acf37a918007ae702e
Opengheimer <intltcsfleader@gmail.com>
2024-05-31 16:38:35 +0700

    fix: stat implementation hotfix

--- resources/js/Components/DivideStats.tsx
+++ resources/js/Components/DivideStats.tsx
@@ -1,4 +1,4 @@
-import { Image, Stack, Text } from "@chakra-ui/react";
+import { Image, Stack, Text, Divider } from "@chakra-ui/react";
 
 export default function DivideStats() {
     return (
@@ -6,6 +6,33 @@ export default function DivideStats() {
             <Stack w={"100vw"}>
                 <Stack>
                     <Image src="/Divide-Stats/divideStats.svg" />
+                </Stack>
+            </Stack>
+
+            <Stack
+                direction={"row"}
+                w={"100vw"}
+                h={"16rem"}
+                bgColor={"#FFF6D9"}
+                align={"center"}
+            >
+                <Stack>
+                    <Text fontWeight={"bold"} fontSize={"6xl"}>
+                        100+
+                    </Text>
+                    <Text fontSize={"3xl"}>Orders/month</Text>
+                </Stack>
+                <Divider
+                    orientation="vertical"
+                    border={"solid 3px"}
+                    rounded={"3xl"}
+                    h={"11rem"}
+                />
+                <Stack>
+                    <Text fontWeight={"bold"} fontSize={"6xl"}>
+                        4.9
+                    </Text>
+                    <Text fontSize={"3xl"}>Orders/month</Text>
                 </Stack>
             </Stack>
         </>
